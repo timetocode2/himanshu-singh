15101535f7f249718100699384953f83374eed02
timetocode2 <timetocode2@gmail.com>
2025-03-04 01:33:20 +0530

    lyrics update

--- spotyphy.js
+++ spotyphy.js
@@ -76,6 +76,7 @@ function validateForm() {
 const contHead = document.getElementById('cont-head');
 const contMain = document.getElementById('cont-main');
 const search = document.getElementById('search');
+const contMenu = document.getElementById('cont-menu');
 
 const myaudio1 = document.getElementById('myaudio1');
 const myaudio2 = document.getElementById('myaudio2');
@@ -100,6 +101,8 @@ const playPauseBtn = document.getElementById('playPauseBtn');
 const seekBar = document.getElementById('seekBar');
 const volumeBar = document.getElementById('volumeBar');
 const songThumb = document.getElementById("song-thumb");
+const closeMusicPlay = document.getElementById("closeMusicPlay");
+
 
 const song1 = document.getElementById("song1");
 const song2 = document.getElementById("song2");
@@ -132,7 +135,7 @@ const songduration = document.getElementById('songduration');
 let anyaudio = "";
 
 function playaudio(audioId, thumbnail) {
-  musicplay.style.setProperty('display', "inherit");
+  musicplay.style.setProperty('display', "flex");
 
   const myaudio = document.getElementById(audioId);
   const close111 = document.querySelectorAll("audio");
@@ -144,7 +147,7 @@ function playaudio(audioId, thumbnail) {
   if (myaudio.paused) {
     myaudio.play();
     playPauseBtn.textContent = 'Pause';
-    musicplay.style.setProperty('display', "inherit");
+    musicplay.style.setProperty('display', "flex");
 
   } else {
     myaudio.pause();
@@ -181,20 +184,21 @@ function playaudio(audioId, thumbnail) {
 };
 
 song1.addEventListener("click", () => {
-  lyricsdivmain.innerHTML = "<span style='color: white;'>Kal the yahi <br> Kyu ab nahi  <br>  Tum aur mai saath dono <br> Kya mil gyi tumko khusi <br> Hoke juda yeh toh bolo <br> Pyar tha waqt nahi <br> jo beet gya do pal me <br> tu banke yaad rahe  <br> fariyad rahega dil me <br>  Socha , kahunga ek din <br> jo tu na mila mujhe <br> jo tu na mila mujhe <br> Dil ko Kya bataunga <br> Dil ko Kya bataunga <br> jo tu na mila mujhe<br> jo tu na mila mujhe <br> Dil ko Kya bataunga <br> Dil ko Kya bataunga  <br> <br>Kah de tu aayega nhi  <br> mujhse mil payega nhi <br> Dekhun kyu rahe mai teri <br> Dil se tu jayega nhi <br> Tu dard samjhega ye nhi <br> Mushkil hai aage Zindgi <br>  <br> Mann tha bada, tu hota mera <br> Tu na mila, ghum hai tera <br>  <br> Kyun dia dard hume <br> hum aaj talak na samjhe <br> Bure hai kya itne  <br> Tum aa na sake jo milne <br> Tu humko bhool gya  <br> Bus yrr hum hi pagal the <br> Sochha Tumhe Jo Raat Din <br> jo tu na mila mujhe <br> jo tu na mila mujhe <br> Dil ko Kya bataunga <br> Dil ko Kya bataunga <br> jo tu na mila mujhe <br> jo tu na mila mujhe <br> Dil ko Kya bataunga <br> Dil ko Kya bataunga  <br> jo tu jo tu na mila mujhe <br> Dil ko Kya bataunga <br> Dil ko Kya bataunga</span>";
-  lyricsdivmain2.innerHTML = `<iframe width="480" height="270" src="https://www.youtube.com/embed/wPqpmsYISBc?si=_OswlQxXZSM1534X" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+  lyricsdivmain1.innerHTML = "<span style='color: white;'>Kal the yahi <br> Kyu ab nahi  <br>  Tum aur mai saath dono <br> Kya mil gyi tumko khusi <br> Hoke juda yeh toh bolo <br> Pyar tha waqt nahi <br> jo beet gya do pal me <br> tu banke yaad rahe  <br> fariyad rahega dil me <br>  Socha , kahunga ek din <br> jo tu na mila mujhe <br> jo tu na mila mujhe <br> Dil ko Kya bataunga <br> Dil ko Kya bataunga <br> jo tu na mila mujhe<br> jo tu na mila mujhe <br> Dil ko Kya bataunga <br> Dil ko Kya bataunga  <br> <br>Kah de tu aayega nhi  <br> mujhse mil payega nhi <br> Dekhun kyu rahe mai teri <br> Dil se tu jayega nhi <br> Tu dard samjhega ye nhi <br> Mushkil hai aage Zindgi <br>  <br> Mann tha bada, tu hota mera <br> Tu na mila, ghum hai tera <br>  <br> Kyun dia dard hume <br> hum aaj talak na samjhe <br> Bure hai kya itne  <br> Tum aa na sake jo milne <br> Tu humko bhool gya  <br> Bus yrr hum hi pagal the <br> Sochha Tumhe Jo Raat Din <br> jo tu na mila mujhe <br> jo tu na mila mujhe <br> Dil ko Kya bataunga <br> Dil ko Kya bataunga <br> jo tu na mila mujhe <br> jo tu na mila mujhe <br> Dil ko Kya bataunga <br> Dil ko Kya bataunga  <br> jo tu jo tu na mila mujhe <br> Dil ko Kya bataunga <br> Dil ko Kya bataunga</span>";
+  lyricsdivmain2.innerHTML =  `Listen Jo tu na mila on yt<iframe width="340" height="192" src="https://www.youtube.com/embed/wPqpmsYISBc?si=_OswlQxXZSM1534X" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
 });
 
 song2.addEventListener("click", () => {
-  lyricsdivmain.innerHTML = " ";
+  lyricsdivmain1.innerHTML = "<span style='color: white;'><span class='headingInLyrics'>[Verse 1]</span><br>Hum tere bin ab reh nahi sakte<br>Tere bina kya wajood mera?<br>Hum tere bin ab reh nahi sakte<br>Tere bina kya wajood mera?<br>Tujhse juda gar ho jayenge<br>To khudse hi ho jayenge juda<br> <br><span class='headingInLyrics'>[Chorus]</span><br>Kyunki tum hi ho, ab tum hi ho<br>Zindagi, ab tum hi ho<br>Chain bhi, mera dard bhi<br>Meri aashiqui, ab tum hi ho<br> <br><span class='headingInLyrics'>[Verse 2]</span><br>Tera mera rishta hai kaisa<br>Ek pal dur gawara nahi<br>Tere liye har roz hai jeete<br>Tujhko diya mera waqt sabhi<br> <br><span class='headingInLyrics'>[Pre-Chorus]</span><br>Koi lamha mera na ho tere bina<br>Har saans pe naam tera<br> <br><span class='headingInLyrics'>[Chorus]</span><br>Kyunki tum hi ho, ab tum hi ho<br>Zindagi, ab tum hi ho<br>Chain bhi, mera dard bhi<br>Meri aashiqui, ab tum hi ho<br> <br><span class='headingInLyrics'>[Bridge]</span><br>Tum hi ho<br>Tum hi ho<br>Tere liye hi jiya main<br>Khudko jo yun de diya hai<br>Teri wafa ne mujhko sambhala<br>Sare ghamon ko dil se nikala<br> <br><span class='headingInLyrics'>[Pre-Chorus]</span><br>Tere saath mera hai naseeb juda<br>Tujhe paake aadura na raha<br> <br><span class='headingInLyrics'>[Chorus]</span><br>Kyunki tum hi ho, ab tum hi ho<br>Zindagi, ab tum hi ho<br>Chain bhi, mera dard bhi<br>Meri aashiqui, ab tum hi ho<br> <br><span class='headingInLyrics'>[Outro]</span><br>Kyunki tum hi ho, ab tum hi ho<br>Zindagi, ab tum hi ho<br>Chain bhi, mera dard bhi<br>Meri aashiqui, ab tum hi ho</span>";
+lyricsdivmain2.innerHTML =  `Listen Tum hi ho on yt<iframe width="340" height="192" src="https://www.youtube.com/embed/asxmdFaIock?si=tTMSwLlZo9jX3pJn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
 });
 
 song3.addEventListener("click", () => {
-  lyricsdivmain.innerHTML = " ";
+  lyricsdivmain1.innerHTML = " ";
 });
 
 song4.addEventListener("click", () => {
-  lyricsdivmain.innerHTML = "<span style='color: white;'>panna ki tamanna hai ke hira mujhe mil jaye<br>chahe meri jaan jaye chahe meraa dil jaye<br>ho panna ki tamanna hai ke hira mujhe mil jaye<br>chahe meri jaan jaye chahe meraa dil jaye<br>haye o ta ra ra ru<br>haye o ta ra ra ru<br>panna ki tamanna hal ke hira mujhe mil jaye<br>chahe meri jaan jaye chahe meraa dil jaye<br> <br>hira toh pehle hi kisi aur kaa ho chuka<br>hira toh pehle hi kisi aur kaa ho chuka<br>kissi ki, madbhari aankho me kho chuka<br>yado ki bas dhul ban chuka dil kaa phul<br>sine pe mai rakh du jo hath<br>phir khil jaye chahe meri jaan jaye chahe meraa dil jaye <br> ho panna ki tamanna hai ke hira mujhe mil jaye<br>chahe meri jaan jaye chahe meraa dil jaye<br> <br> dil toh dete hai lete hai log kayi baar <br> dil toh dete hai lete hai log kayi baar <br>huwa kya, kisi se, kiya tha tumne pyar<br>yado ko chhod de, wado ko tod de<br>apni  jagah se kaise parbat hil jaye<br>chahe meri jaan jaye chahe meraa dil jaye<br> <br> bhula naa mere dil ko kabhi jis kaa khayal<br>bhula naa mere dil ko kabhi jis kaa khayal<br>ho sake, toh use, mere dil se tu nikal<br>naa karu mai yeh kam toh nahi meraa naam<br>naa karu mai yeh kam toh nahi meraa naam<br>baton se yeh jakhme-jigar kaise sil jaye<br>chahe meri jaan jaye chahe meraa dil jaye<br>ho panna ki tamanna hai ke hira mujhe mil jaye<br>chahe meri jaan jaye chahe meraa dil jaye<br>ho le lo, ta ra ra ru, ho le lo, ta ra ra ru</span>";
+  lyricsdivmain1.innerHTML = "<span style='color: white;'>panna ki tamanna hai ke hira mujhe mil jaye<br>chahe meri jaan jaye chahe meraa dil jaye<br>ho panna ki tamanna hai ke hira mujhe mil jaye<br>chahe meri jaan jaye chahe meraa dil jaye<br>haye o ta ra ra ru<br>haye o ta ra ra ru<br>panna ki tamanna hal ke hira mujhe mil jaye<br>chahe meri jaan jaye chahe meraa dil jaye<br> <br>hira toh pehle hi kisi aur kaa ho chuka<br>hira toh pehle hi kisi aur kaa ho chuka<br>kissi ki, madbhari aankho me kho chuka<br>yado ki bas dhul ban chuka dil kaa phul<br>sine pe mai rakh du jo hath<br>phir khil jaye chahe meri jaan jaye chahe meraa dil jaye <br> ho panna ki tamanna hai ke hira mujhe mil jaye<br>chahe meri jaan jaye chahe meraa dil jaye<br> <br> dil toh dete hai lete hai log kayi baar <br> dil toh dete hai lete hai log kayi baar <br>huwa kya, kisi se, kiya tha tumne pyar<br>yado ko chhod de, wado ko tod de<br>apni  jagah se kaise parbat hil jaye<br>chahe meri jaan jaye chahe meraa dil jaye<br> <br> bhula naa mere dil ko kabhi jis kaa khayal<br>bhula naa mere dil ko kabhi jis kaa khayal<br>ho sake, toh use, mere dil se tu nikal<br>naa karu mai yeh kam toh nahi meraa naam<br>naa karu mai yeh kam toh nahi meraa naam<br>baton se yeh jakhme-jigar kaise sil jaye<br>chahe meri jaan jaye chahe meraa dil jaye<br>ho panna ki tamanna hai ke hira mujhe mil jaye<br>chahe meri jaan jaye chahe meraa dil jaye<br>ho le lo, ta ra ra ru, ho le lo, ta ra ra ru</span>";
 });
 
 
@@ -226,33 +230,113 @@ volumeBar.addEventListener('input', () => {
 
 
 
+closeMusicPlay.addEventListener('click', () => {
+
+  if (lyricsdivmain1.style.display === "inherit") {
+    musicplay.style.removeProperty('top', "10px");
+    contHead.style.setProperty('display', 'flex');
+    contMain.style.setProperty('display', 'flex');
+    search.style.setProperty('display', 'inherit');
+    lyricsdivclose.style.setProperty('display', "none");
+    fullLyrics.style.setProperty('display', "none");
+    lyricsdivmain1.style.setProperty('display', "none");
+    lyricsdivmain2.style.setProperty('display', "none");
+    }else{
+  musicplay.style.setProperty('display', "none");
+  anyaudio.pause();
+  playPauseBtn.textContent = 'Play';
+  musicplay.style.removeProperty('top', "10px");
+  contHead.style.setProperty('display', 'flex');
+  contMain.style.setProperty('display', 'flex');
+  search.style.setProperty('display', 'inherit');
+  lyricsdivclose.style.setProperty('display', "none");
+  fullLyrics.style.setProperty('display', "none");
+  lyricsdivmain1.style.setProperty('display', "none");
+  lyricsdivmain2.style.setProperty('display', "none");
+    };
+
+
+    if(fullLyrics.textContent === "Short Lyrics"){
+      lyricsdivclose.style.removeProperty('top', "0");
+      fullLyrics.style.removeProperty('top', "0");
+      divExtraBali.style.removeProperty('top', "0");
+      lyricsdivmain2.style.removeProperty('top', "0");
+      lyricsdivmain1.style.removeProperty('top', "10px");
+      lyricsdivmain1.style.removeProperty('width', "100%");
+      lyricsdivmain1.style.removeProperty('height', "100%");
+      fullLyrics.textContent = "Full Lyrics";}
+  
+});
+
 
 const lyrics = document.getElementById('lyrics');
-const lyricsdiv = document.getElementById('lyricsdiv');
 const lyricsdivclose = document.getElementById('lyricsdivclose');
-const lyricsdivmain = document.getElementById('lyricsdivmain');
+const lyricsdivmain1 = document.getElementById('lyricsdivmain1');
 const lyricsdivmain2 = document.getElementById('lyricsdivmain2');
+const fullLyrics = document.getElementById('fullLyrics');
+const divExtraBali = document.querySelector('.divExtraBali');
 
 
 lyrics.addEventListener('click', () => {
-
-  musicplay.style.setProperty('position', "fixed");
-  musicplay.style.setProperty('top', "-85vh");
-  contHead.style.setProperty('visibility', 'hidden');
-  contMain.style.setProperty('visibility', 'hidden');
-  search.style.setProperty('visibility', 'hidden');
-  lyricsdiv.style.setProperty('display', "inherit");
+  musicplay.style.setProperty('top', "10px");
+  contHead.style.setProperty('display', 'none');
+  contMain.style.setProperty('display', 'none');
+  search.style.setProperty('display', 'none');
+  lyricsdivmain1.style.setProperty('display', "inherit");
+  lyricsdivmain2.style.setProperty('display', "inherit");
+  lyricsdivclose.style.setProperty('display', "flex");
+  fullLyrics.style.setProperty('display', "flex");
 });
 
 lyricsdivclose.addEventListener('click', () => {
-  musicplay.style.setProperty('position', "static");
-  musicplay.style.setProperty('top', "0");
-  contHead.style.setProperty('visibility', 'visible');
-  contMain.style.setProperty('visibility', 'visible');
-  search.style.setProperty('visibility', 'visible');
-  lyricsdiv.style.setProperty('display', "none");
+  musicplay.style.removeProperty('top', "10px");
+  contHead.style.setProperty('display', 'flex');
+  contMain.style.setProperty('display', 'flex');
+  search.style.setProperty('display', 'inherit');
+  lyricsdivclose.style.setProperty('display', "none");
+  fullLyrics.style.setProperty('display', "none");
+  lyricsdivmain1.style.setProperty('display', "none");
+  lyricsdivmain2.style.setProperty('display', "none");
+
+  if(fullLyrics.textContent === "Short Lyrics"){
+    lyricsdivclose.style.removeProperty('top', "0");
+    fullLyrics.style.removeProperty('top', "0");
+    divExtraBali.style.removeProperty('top', "0");
+    lyricsdivmain2.style.removeProperty('top', "0");
+    lyricsdivmain1.style.removeProperty('top', "10px");
+    lyricsdivmain1.style.removeProperty('width', "100%");
+    lyricsdivmain1.style.removeProperty('height', "100%");
+    fullLyrics.textContent = "Full Lyrics";}
+});
+
+fullLyrics.addEventListener('click', () => {
+  if(fullLyrics.textContent === "Full Lyrics"){
+  musicplay.style.removeProperty('top', "0px");
+  lyricsdivclose.style.setProperty('top', "0");
+  fullLyrics.style.setProperty('top', "0");
+  divExtraBali.style.setProperty('top', "0");
+  lyricsdivmain2.style.setProperty('top', "0");
+  lyricsdivmain1.style.setProperty('top', "0");
+  lyricsdivmain1.style.setProperty('width', "90%");
+  lyricsdivmain1.style.setProperty('height', "100%");
+  fullLyrics.textContent = "Short Lyrics";}
+  else{
+    musicplay.style.setProperty('top', "10px");
+    lyricsdivclose.style.removeProperty('top', "0");
+    fullLyrics.style.removeProperty('top', "0");
+    divExtraBali.style.removeProperty('top', "0");
+    lyricsdivmain2.style.removeProperty('top', "0");
+    lyricsdivmain1.style.removeProperty('top', "10px");
+    lyricsdivmain1.style.removeProperty('width', "100%");
+    lyricsdivmain1.style.removeProperty('height', "100%");
+    fullLyrics.textContent = "Full Lyrics";
+  }
 
 });
+
+
+
+
 
 
 const contHome = document.getElementById('cont-home');
